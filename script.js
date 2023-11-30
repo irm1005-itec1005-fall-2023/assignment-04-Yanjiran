48174b1e6541d532a54c0b0f06c0c6771b36e40c
Zi Ran Yang <killerbuster10@gmail.com>
2023-11-29 19:13:20 -0500

    i did things

--- script.js
+++ script.js
@@ -6,74 +6,71 @@
 // Variables
 const todoTasks = [];
 
-const todoList = document.querySelector("todo-list-ul");
+const todoList = document.querySelector("#todo-list-ul");
 
-const todoForm = document.querySelector("todoInputBox");
+const todoForm = document.querySelector(".todoInputBox");
 
-const todoInput = document.querySelector("todo-input");
+const todoInput = document.querySelector("#todo-input");
 
 
 
 
 
 // Functions
-
 function addTaskItem(event) {
     event.preventDefault();
     const taskInput = todoInput.value;
     todoTasks.push(taskInput);
-    rednerList();
+    renderList();
     todoForm.reset();
-}
-
-
-function createTodoItem() {
+  }
+  
+  function createTodoItem(taskInput) {
     // Create <li> element
     let listItem = document.createElement('li');
     listItem.className = 'todo-item-li';
-
-    // Create the <div> element with class 'task-item'
+  
+    // Create <div> with class 'task-item'
     let taskItemDiv = document.createElement('div');
     taskItemDiv.className = 'task-item';
-
-    // Create the <p> element with class 'todo-item-content'
+    
+    // Create <p> with class 'todo-item-content'
     let todoItemContent = document.createElement('p');
     todoItemContent.className = 'todo-item-content';
-    todoItemContent.textContent = todoInput; // You can change this text
-
-    // Create the <input> element with type 'button' and class 'task-delete'
+    todoItemContent.textContent = taskInput; // Use taskInput, not todoInput
+  
+    // Create <input> with type 'button' and class 'task-delete'
     let deleteButton = document.createElement('input');
     deleteButton.type = 'button';
     deleteButton.className = 'task-delete';
     deleteButton.value = 'delete-item';
-
-    // Append the <p> and <input> elements to the <div> element
+  
+    // Append <p> and <input> to <div>
     taskItemDiv.appendChild(todoItemContent);
     taskItemDiv.appendChild(deleteButton);
-
-    // Append the <div> element to the <li> element
+  
+    // Append <div> to <li>
     listItem.appendChild(taskItemDiv);
-
-    // Append the <li> element to the <ul> (assuming your <ul> has an id of 'todo-list')
-    document.getElementById('todo-list').appendChild(listItem);
-}
-
-}
-
-
-function renderList() {
-    while(todoList.firstChild) {
-        todoList.removeChild(todoList.firstChild)
+  
+    return listItem;
+  }
+  
+  function renderList() {
+    while (todoList.firstChild) {
+      todoList.removeChild(todoList.firstChild);
     }
-
-    for(let i = 0; i < todoTasks.length; i++) {
-        const listItem = document.createElement(createTodoItem())
-
-        if(i === todoTasks.length - 1) {
-            listItem.classList.add("new-item-annimate");
-        }
-
-        todoList.appendChild(listItem);
+  
+    for (let i = 0; i < todoTasks.length; i++) {
+      let listItem = createTodoItem(todoTasks[i]);
+  
+      if (i === todoTasks.length - 1) {
+        listItem.classList.add("new-item-animate");
+      }
+  
+      todoList.appendChild(listItem);
     }
-}
-
+  }
+  
+  todoForm.addEventListener("submit", addTaskItem);
+  
+  renderList();

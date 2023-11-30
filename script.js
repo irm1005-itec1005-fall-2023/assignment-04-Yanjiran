20bf9c1fdd193bf9e9e3d4cbbf246308b803862b
Zi Ran Yang <killerbuster10@gmail.com>
2023-11-29 23:01:09 -0500

    edited JS to fix broken things

--- script.js
+++ script.js
@@ -9,34 +9,38 @@ const todoList = document.querySelector("#todo-list-ul");
 const todoForm = document.querySelector(".todoInputBox");
 const todoInput = document.querySelector("#todo-input");
 
-
 //========= Functions ============//
-  // Adds new task
+// Adds new task
 function addTaskItem(event) {
   event.preventDefault();
   const taskInput = todoInput.value;
-  todoTasks.push(taskInput);
+  todoTasks.push({ text: taskInput, completed: false });
   renderList();
   todoForm.reset();
 }
 
-  // Create task item in HTML
-function createTodoItem(taskInput) {
+// Create task item in HTML
+function createTodoItem(task) {
   // Create <li> element
   let listItem = document.createElement('li');
   listItem.className = 'todo-item-li';
-  
+
   // Create <p> with class 'todo-item-content'
   let todoItemContent = document.createElement('p');
   todoItemContent.className = 'todo-item-content';
-  todoItemContent.textContent = taskInput; // Use taskInput, not todoInput
-  
+  todoItemContent.textContent = task.text;
+
+  // Set completion status
+  if (task.completed) {
+    todoItemContent.classList.add('task-complete');
+  }
+
   // Create <input> with type 'button' and class 'task-delete'
   let deleteButton = document.createElement('input');
   deleteButton.type = 'button';
   deleteButton.className = 'task-delete';
   deleteButton.value = 'delete-item';
-  
+
   // Append <p> and <input> to <li>
   listItem.appendChild(todoItemContent);
   listItem.appendChild(deleteButton);
@@ -49,28 +53,15 @@ function renderList() {
   while (todoList.firstChild) {
     todoList.removeChild(todoList.firstChild);
   }
-  
+
   for (let i = 0; i < todoTasks.length; i++) {
     let listItem = createTodoItem(todoTasks[i]);
-    
+
     if (i === todoTasks.length - 1) {
       listItem.classList.add("new-item-animate");
     }
 
-    todoList.appendChild(listItem);                             
-  }
-}
-
-function removeTask(index) {
-  // Preserve completion status before removing the task
-  const isCompleted = todoList.children[index].querySelector('.todo-item-content').classList.contains('task-complete');
-  todoTasks.splice(index, 1);
-  renderList();
-
-  // If the task was completed, reapply the completion status
-  if (isCompleted) {
-    const lastTask = todoList.children[todoList.children.length - 1];
-    lastTask.querySelector('.todo-item-content').classList.add('task-complete');
+    todoList.appendChild(listItem);
   }
 }
 
@@ -80,8 +71,9 @@ function handleDeleteButtonClick(event) {
     // Get the index of the task in the list
     const index = Array.from(event.target.closest('ul').children).indexOf(event.target.closest('li'));
 
-    // Remove the task without affecting completion status
-    removeTask(index);
+    // Remove the task
+    todoTasks.splice(index, 1);
+    renderList();
   }
 }
 
@@ -91,18 +83,21 @@ function markTaskComplete(event) {
 
   // Check if the clicked target has the class 'todo-item-content'
   if (clickedElement.classList.contains('todo-item-content')) {
-    // Toggle the background color when clicked
-    clickedElement.classList.toggle('task-complete');
+    // Find the parent <li> and get its index
+    const index = Array.from(clickedElement.closest('ul').children).indexOf(clickedElement.closest('li'));
+
+    // Toggle the completion status in the todoTasks array
+    todoTasks[index].completed = !todoTasks[index].completed;
+
+    // Update the rendering of the list
+    renderList();
   }
 }
-
-
 
 //========= Event Listeners ===========//
 todoForm.addEventListener("submit", addTaskItem);
 todoList.addEventListener('click', handleDeleteButtonClick);
 todoList.addEventListener('click', markTaskComplete);
-
 
 //======= Initialization/Execution =========//
 renderList();
